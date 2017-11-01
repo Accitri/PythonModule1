3b8ffe8573969e45cde4f54ab1ed02ce1f6f4667
Accitri <preben@noramix.eu>
2017-11-01 20:34:29 +0100

    using user inputs

---
+++ InputStatement.py
@@ -0,0 +1,10 @@
+
+#here we want to take an input from the user
+
+print("Please enter the radius of the circle :")
+radius = float(input())
+
+print("You have entered a radius of", radius)
+
+myConstPi = 3.14
+print("The circle's circumference is :", 2*myConstPi*radius)
