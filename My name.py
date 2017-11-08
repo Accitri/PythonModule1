68aa461f841d76b421eabe533e408edffd88629d
Accitri <Preben@noramix.eu>
2017-11-08 18:16:32 +0100

    My name, and age.

---
+++ My name.py
@@ -0,0 +1,14 @@
+#My name
+print("My name is Preben.")
+
+#My age
+age = 16
+
+print("My age is", age)
+
+#My age in 2077
+year = 2077
+birthYear = 2001
+
+print("My age in 2077, will be", year - birthYear)
+
