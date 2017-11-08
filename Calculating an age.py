56870c3a9abcbe9bfd4990f78562100e63835453
Accitri <Preben@noramix.eu>
2017-11-08 18:12:42 +0100

    Program for calculating the user's age

---
+++ Calculating an age.py
@@ -0,0 +1,11 @@
+#Calculating an age
+year = int(input("Give me a year:"))
+
+yearOfBirth = int(input("Give me your year of birth:"))
+
+print("Your age is:", year-yearOfBirth)
+
+#Tests
+# 1) Entered a string - breaks
+# 2) Enter a decimal - breaks
+# 3) Empty string - breaks
