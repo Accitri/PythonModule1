906895ca6ff1db49ebb268c02994541a1e558349
Accitri <Preben@noramix.eu>
2017-11-08 19:28:16 +0100

    Added conditions

--- Calculating an age.py
+++ Calculating an age.py
@@ -1,9 +1,37 @@
 #Calculating an age
-year = int(input("Give me a year:"))
 
-yearOfBirth = int(input("Give me your year of birth:"))
+yearOfBirth = int(input("What is your year of birth?"))
 
-print("Your age is:", year-yearOfBirth)
+currentYear = int(input("What is the current year?"))
+
+#Prevents the user from being born after the current year
+if(yearOfBirth > currentYear):
+    print("You can not be born in the future.")
+    exit(0)
+
+year = int(input("Give me a year (,above 1900):"))
+
+#The submitted year, can not be less than 1900
+if(year <= 1900):
+    print("The submitted year, is not valid.")
+    exit(0)
+
+#Prevents the user from being born after the submitted year
+if(yearOfBirth > year):
+    print("You can not be born, before the submitted year.")
+    exit(0)
+
+#The user's age
+age = year - yearOfBirth
+
+if(age > 120):
+    print("That is too old.")
+    exit(0)
+
+print("You are", age, "years old.")
+
+
+
 
 #Tests
 # 1) Entered a string - breaks
