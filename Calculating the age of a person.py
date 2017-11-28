8ed6d144f03f76f14c15ad17a50757aaa3e3f7f4
Accitri <preben@noramix.eu>
2017-11-28 20:55:03 +0100

    Added:
    If the age of a person is exactly 100, greet them.
    If the age of a person is 1, wish them on their first birthday.
    If the age comes up between 13 and 19, say hello to the teenager
    Calculate the sum of the digits of the person's birth year.

--- Calculating the age of a person.py
+++ Calculating the age of a person.py
@@ -1,8 +1,9 @@
 #Calculating an age
 
-yearOfBirth = int(input("What is your year of birth?"))
+yearOfBirth = int(input("What is your year of birth? "))
 
-currentYear = int(input("What is the current year?"))
+currentYear = int(input("What is the current year? "))
+
 
 #Prevents the user from being born after the current year
 if(yearOfBirth > currentYear):
@@ -11,28 +12,49 @@ if(yearOfBirth > currentYear):
 
 year = int(input("Give me a year (,above 1900):"))
 
+
 #The submitted year, can not be less than 1900
 if(year <= 1900):
     print("The submitted year, is not valid.")
     exit(0)
 
+
 #Prevents the user from being born after the submitted year
 if(yearOfBirth > year):
-    print("You can not be born, before the submitted year.")
+    print("You can not be born before your year of birth.")
     exit(0)
+
 
 #The user's age
 age = year - yearOfBirth
+
+if(age == 1):
+    print("You are", age, "year old.")
+
+if(age > 1):
+    print("You are", age, "years old.")
 
 if(age > 120):
     print("That is too old.")
     exit(0)
 
-print("You are", age, "years old.")
 
+#Messages if the receiver is certain ages.
 if(age == 0):
+    print("You are a newborn. Good luck in life!")
+
+if(age == 1):
+    print("Congratulations on completing your first year alive!")
+
+if(age == 100):
+    print("You are a centenarian. Congratulations on becoming one of 0.005% oldest living people in the world!")
+
+if(13 <= age <= 19):
+    print("You are a teenager. Enjoy your youth - you won't have the chance to relive it.")
 
 
+#Sum of the digits of the person's birth year
+sum(map(int, str(3)))
 
 
 #Tests
