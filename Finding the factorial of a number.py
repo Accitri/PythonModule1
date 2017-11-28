9fa2e8db92c8b8eb7d31492bdd0d25e2f8b2c827
Accitri <preben@noramix.eu>
2017-11-28 20:58:10 +0100

    Made a program, that calculates the factorial of a given number.

---
+++ Finding the factorial of a number.py
@@ -0,0 +1,9 @@
+
+myNumber = int(input("Submit a number: "))
+
+# noinspection PyUnresolvedReferences
+from math import factorial
+
+myFactorial = (factorial(myNumber))
+
+print("The factorial of", myNumber, "is",myFactorial )
