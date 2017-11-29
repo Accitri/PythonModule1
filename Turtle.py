8bca5ee698ea367137686e85a97f436d7f4c5063
Accitri <Preben@noramix.eu>
2017-11-29 18:56:08 +0100

    making a turtle walk in the pattern of two right triangles, making a square

---
+++ Turtle.py
@@ -0,0 +1,23 @@
+
+import turtle
+
+maxCounter = 1
+counter = 0
+myTurtle = turtle.Turtle()
+
+while (counter < maxCounter):
+    myTurtle.right(45)
+    myTurtle.forward(105)
+    myTurtle.right(135)
+    myTurtle.forward(75)
+    myTurtle.right(90)
+    myTurtle.forward(75)
+    myTurtle.right(90)
+    myTurtle.forward(75)
+    myTurtle.right(90)
+    myTurtle.forward(75)
+
+
+    counter = counter + 1
+
+turtle.done()
