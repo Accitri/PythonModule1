f471f4964e72e598981dfb275ceaedaa47aaa50b
Accitri <Preben@noramix.eu>
2017-12-06 20:11:34 +0100

    added an alternative function

--- 06.12.2017/Class work/dataVisualisationWithTurtle.py
+++ 06.12.2017/Class work/dataVisualisationWithTurtle.py
@@ -2,9 +2,27 @@
 
 import turtle
 
-myTurtle = turtle.Turtle()
-averageTemperatureList = [3, 5, 1, -4, -1, 4, 0, -5, -1, -3, 1, 4]
+myTurtle = turtle.Turtle
 
+#defining a function for the basic setup of the turtle
+def setupTurtle():
+    myTurtleInsideFunction = turtle.Turtle()
+    myTurtleInsideFunction.penup()
+    myTurtleInsideFunction.setpos(-300, 0)
+    myTurtleInsideFunction.pendown()
+    myTurtleInsideFunction.color('red')
+    myTurtleInsideFunction.pensize(3)
+
+    return myTurtleInsideFunction
+
+
+#calling the setupTurtle function and
+#store the result in a variable called myTurtle
+myTurtle = setupTurtle()
+
+
+#define the temperature list
+averageTemperatureList = [3, 5, 1, -4, -1, 4, 0, -5, -1, -3, 1, 4]
 
 #defining a function that draws a rectangle
 def drawGraphRectangle():
@@ -13,17 +31,47 @@ def drawGraphRectangle():
             myTurtle.color('green')
         if (averageTemperatureList[i] < 0):
             myTurtle.color('red')
-        myTurtle.forward(10)
+        myTurtle.forward(20)
         myTurtle.left(90)
-        myTurtle.forward(averageTemperatureList[i] * 10)
+        myTurtle.forward(averageTemperatureList[i] * 20)
         myTurtle.right(90)
-        myTurtle.forward(10)
+        myTurtle.forward(20)
         myTurtle.right(90)
-        myTurtle.forward(averageTemperatureList[i] * 10)
+        myTurtle.forward(averageTemperatureList[i] * 20)
         myTurtle.left(90)
 
 
+#defining function that draws a rectangle
+def pulse(height, width):
+    for i in range(0, len(averageTemperatureList)):
+        if (averageTemperatureList[i] >= 0):
+            myTurtle.color('green')
+        if (averageTemperatureList[i] < 0):
+            myTurtle.color('red')
+        myTurtle.left(90)
+        myTurtle.forward(height * 10)
+        myTurtle.right(90)
+        myTurtle.forward(width)
+        myTurtle.right(90)
+        myTurtle.forward(height * 10)
+        myTurtle.left(90)
+        myTurtle.forward(width)
+
+#arguments
+#for temp in averageTemperatureList[i]:
+    #pulse(temp, 25)
+
+#pulse()
+
+
+
+
+
+
+#calling the drawGraphRectangle function
+#to visualise averageTemperatureList
 drawGraphRectangle()
+
 
 turtle.done()
 
