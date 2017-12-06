d3ccdd5a023332ff801c96d884bbf5574ab48899
Accitri <Preben@noramix.eu>
2017-12-06 19:27:51 +0100

    added colours to the graph if the temperature is above or below 0 degrees

--- 06.12.2017/Class work/dataVisualisationWithTurtle.py
+++ 06.12.2017/Class work/dataVisualisationWithTurtle.py
@@ -3,12 +3,16 @@
 import turtle
 
 myTurtle = turtle.Turtle()
-averageTemperatureList = [3, 6, 9, 14, 17, 18, 17, 14, 11, 7, 4]
+averageTemperatureList = [3, 5, 1, -4, -1, 4, 0, -5, -1, -3, 1, 4]
 
 
 #defining a function that draws a rectangle
 def drawGraphRectangle():
     for i in range(0, len(averageTemperatureList)):
+        if (averageTemperatureList[i] >= 0):
+            myTurtle.color('green')
+        if (averageTemperatureList[i] < 0):
+            myTurtle.color('red')
         myTurtle.forward(10)
         myTurtle.left(90)
         myTurtle.forward(averageTemperatureList[i] * 10)
