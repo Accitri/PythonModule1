dbd9b1e81337ecdb42cf8b358ed583c76494eabd
Accitri <Preben@noramix.eu>
2017-12-13 21:03:21 +0100

    added missing code

--- 13.12.2017/Class work/credit card validator.py
+++ 13.12.2017/Class work/credit card validator.py
@@ -17,9 +17,13 @@ def doubleAlternateDigits(number):
     for i in range(0, len(number)):
         if(i % 2 == 0):
             doubleOfDigits.append(int(number[i])*2)
+        else:
+            doubleOfDigits.append((int(number[i])))
 
 doubleAlternateDigits(myCreditCardNumber)
 print(doubleOfDigits)
+
+print("double of alternate digits is", doubleOfDigits)
 
 def addDigits(number):
     sumOfDigits = 0
