7015fad589a2f2c8fcce3eb503ed36cf9da61a08
Accitri <Preben@noramix.eu>
2018-01-10 21:01:53 +0100

    Functions for reading and writing to files, creating files, and checking if items are in another file

---
+++ 10.1.2018/Class work/FileOperations.py
@@ -0,0 +1,63 @@
+
+
+#reading a file
+
+def readMyDataFile(dataFileName):
+    with open(dataFileName, mode = 'r') as myDataFile:
+        items = myDataFile.read().splitlines()
+        return items
+
+#readMyDataFile()
+
+#writing to a file
+
+def writeMyDataFile(dataFileName):
+    with open(dataFileName, mode = 'w') as myDataFile:
+        myDataFile.write(input("write something: "))
+
+#writeMyDataFile('shopping.txt')
+
+
+#creating a new file, and inserting a list there
+
+shoppingList = ['Apple', 'Soap', 'Tacos']
+
+def writeToDataToMyFile(dataFileName):
+    with open(dataFileName, mode = 'x') as myDataFile:
+        for item in shoppingList:
+            myDataFile.write(item+'\n')
+
+#writeToDataToMyFile('something.txt')
+
+
+#checking if an item is inside a file
+
+def seeIfItemExists(myItem):
+    myShoppingList = readMyDataFile('shopping.txt')
+    for item in myShoppingList:
+        if (item == myItem):
+            print(myItem, "exists in shopping list")
+            return
+
+    print(myItem, "does not exist in shopping list")
+
+#seeIfItemExists("Orange")
+
+
+def checkPassword(userName):
+    data = readMyDataFile("userCredentials.txt")
+    for user in data:
+        userCred = user.split(';')
+        if (userCred[0] == userName):
+            print("Type password for", userName)
+            userPass = input()
+            if (userPass == userCred[1]):
+                print("The Password is correct")
+            else:
+                print("The password for", userName, "is incorrect")
+
+
+checkPassword(input("Type your username"))
+
+
+
