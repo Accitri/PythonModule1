f950a521b29c26da918d671c0623c9d828fe4399
Accitri <Preben@noramix.eu>
2018-01-10 21:02:59 +0100

    function for calculating the area of a rectangle

---
+++ 10.1.2018/Class work/area of a rectangle.py
@@ -0,0 +1,7 @@
+
+
+def areaRect(l,w):
+    area = l*w
+    print("The rectangle's area is", area)
+
+areaRect(int(input("What is the rectangle's length? ")), int(input("What is the rectangle's width? ")))
